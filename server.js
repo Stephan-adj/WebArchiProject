c3aa4fb0a29b6ae9a1508b426de8e08fd4d30be1
Stephan-adj <stephan.adjarian@free.fr>
2021-03-16 15:13:19 +0100

    My first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = process.env.PORT;
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+   var regip = req.ip;
+   if (regip.substr(0, 7) == "::ffff:") {
+      regip=regip.substr(7)
+   }
+   console.log(regip);
+   res.send('Hello World');
+
+});
+
+app.listen(PORT, console.log(`Running on port ${PORT}`)  );
