fc5483dec4dcf635affb917bf69fb3b1760c3f71
Stéphan <62169405+Stephan-adj@users.noreply.github.com>
2021-04-05 00:16:00 +0200

    Update script.js

--- ASSIGNEMENT 1/public/script.js
+++ ASSIGNEMENT 1/public/script.js
@@ -95,6 +95,17 @@ function to_image(){
   win.document.write('<h1 style=text-align:center>Voici votre dessin</h1><div><iframe src="' + dataUrl  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe></div>');
 }
 
+//To submit the user name when pressing enter key (user name needs to have more than 3 letters) 
+var input = document.getElementById("username_field");
+input.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+   event.preventDefault();
+   document.getElementById("submit_username").click();
+  }
+});
+
+
+
 //FIGURE
 
 function draw(){
