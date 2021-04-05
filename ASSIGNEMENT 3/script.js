308b4d68fd1f8948938c30a953f4c5366b19ea11
Stéphan <62169405+Stephan-adj@users.noreply.github.com>
2021-04-05 04:27:56 +0200

    Update script.js

--- ASSIGNEMENT 3/script.js
+++ ASSIGNEMENT 3/script.js
@@ -14,9 +14,11 @@ const Retrievemovieinfo = function(url) {
                   `<div id='title'><div> Title : ${movieInfos.title}</div>` +
                   `<div> <img src='https://image.tmdb.org/t/p/w500${movieInfos.poster_path}'></div></div>` +
                   `<div> Release year : ${movieInfos.release_date}</div>` +
-                  "<input type='text' id='name_field' placeholder='Please input director or actor of the movie'>" +
-                  "<button id='submit_username' onclick='test()'>Submit</button>";
-                  //"<a href='#' onclick='Submit(\"${movieInfos.id}\")' class='buttonContainer'>Submit my guess</a></div>";
+                  `<input type='text' id='name_field' placeholder='Please input director or actor of the movie'>` +
+                  `<button id='submit_username' onclick='ValidateActor(\"${movieInfos.id}\")'>Submit</button>` +
+                  `<div id='previous1'></div>`;
+
+
                   // Add a way for the user to submit form by pressing enter key
                   var input = document.getElementById("name_field");
                   input.addEventListener("keyup", function(event) {
@@ -25,19 +27,57 @@ const Retrievemovieinfo = function(url) {
                      document.getElementById("submit_username").click();
                     }
                   });
-
             });
         }
     });
 }
 
-const start = function() {
-  const result = Retrievemovieinfo(url);
+const ValidateActor = async (movideId) => {
+  // Remove enter key for previous submission form.
+  document.getElementById("name_field").removeEventListener('keyup', function(event) {
+    if (event.keyCode === 13) {
+     event.preventDefault();
+     document.getElementById("submit_username").click();
+    }
+  });
 
+  console.log("TEST : "+ movideId);
+  var name = document.getElementById("name_field").value;
+  console.log(name);
+  var url = `https://api.themoviedb.org/3/movie/${movideId}/credits?api_key=5a905ae022c5ec2582edbfac057517d8`;
+  console.log(url);
+  const actors = await RetrieveActor(name, url);
+  console.log(actors);
+  if (actors != null){
+    document.getElementById("previous1").innerHTML = "";
+    document.body.innerHTML +=
+    `<div id='name'> Name : ${actors[0]}<br></br><img src='https://image.tmdb.org/t/p/w500${actors[1]}'></div>` +
+    `<input type='text' id='name_field' placeholder='Please input director or actor of the movie'>` +
+    `<button id='submit_username' onclick='ValidateActor(\"${actors[2]}\")'>Submit</button>`;
+  }
+  else{
+    document.getElementById("previous1").innerHTML = "<p id='wrong' style='color: red'> You put the wrong actor.</p>";
+  }
 }
 
-const test = function() {
-  console.log("TEST : ");
+const RetrieveActor = async (name, url) => {
+  return fetch(url)
+  .then(res => res.json())
+      .then(json => {
+          actors = json.cast;
+          for (j = 0; j < actors.length; j++) {
+              if (actors[j].name.toLowerCase() == name.toLowerCase()) {
+                  return [actors[j].name, actors[j].profile_path, actors[j].id];
+              }
+          }
+          return null;
+      })
+}
+
+
+
+const start = function() {
+  const result = Retrievemovieinfo(url);
 }
 
 start();
